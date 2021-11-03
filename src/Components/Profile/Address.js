dc0fc2d0f11fe0e13adb1d50463a0353186d9340
Your Name <guesmirayen9@gmail.com>
2021-11-03 19:20:07 +0100

    first commit

---
+++ src/Components/Profile/Address.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Address extends Component {
+    render() {
+        return (
+            <div>
+        <p className="my-address">33 Rue d'orange,Fouchena,Ben arous</p>
+            </div>
+        )
+    }
+}
---
+++ src/Components/Profile/FullName.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Address extends Component {
+    render() {
+        return (
+            <div>
+         <p className="my-name">Rayen Guesmi</p>
+         
+            </div>
+        )
+    }
+}
